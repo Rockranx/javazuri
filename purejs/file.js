a4d41d0ecbd31c3e8ffd9ff5230fd885dd9b5504
Rockranx <sughtera@gmail.com>
2022-06-22 19:52:35 +0100

    update

---
+++ purejs/file.js
@@ -0,0 +1,13 @@
+console.log(
+    'My name is Achi Sughter Tooyosi'
+)
+
+console.log(
+    'I am 1.5m in height'
+)
+
+console.log(
+    'I am a Nigerian'
+)
+// cd into directory
+//run node file.js
